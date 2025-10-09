28030700f129b79fd4b1513bbc6e5b7b4b32cb54
Alan Quintero <alan_q_b@hotmail.com>
2025-10-09 13:04:11 -0600

    Set up Spring Boot project from scratch and add Maven Wrapper

---
+++ src/main/java/com/moviereviews/MovieReviewsApplication.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2025 Alan Quintero
+ * Source: https://github.com/alanquintero/movie-reviews
+ */
+package com.moviereviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MovieReviewsApplication.class, args);
+    }
+}
